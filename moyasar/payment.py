f91aebf3ff9c286f88c6e45ca8c040d3cfbabbee
nuha <nuhaaziz75@gmail.com>
2019-02-12 13:44:07 +0300

    Constructed new class Source, a parent of Sadad and CreditCard + changes for Source

--- moyasar/payment.py
+++ moyasar/payment.py
@@ -2,7 +2,19 @@ from moyasar.resource import Resource
 from moyasar.actions.refund import Refund
 
 
-class Payment(Resource, Refund):
+class Source:
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    @classmethod
+    def build(cls, payment):
+        if payment.source['type'] == "creditcard":
+            payment.source = CreditCard.source_to_creditcard(payment.source)
+        else:
+            payment.source = Sadad.source_to_sadad(payment.source)
+        return payment
+
     @classmethod
     def source_to_creditcard(cls, data):
         data.pop('type')
@@ -13,41 +25,31 @@ class Payment(Resource, Refund):
         data.pop('type')
         return Sadad(**data)
 
-    @classmethod
-    def fix_source(cls, payment):
-        if payment.source['type'] == 'creditcard':
-            payment.source = cls.source_to_creditcard(payment.source)
-        else:
-            payment.source = cls.source_to_sadad(payment.source)
-        
-        return payment
+
+class CreditCard(Source):
+    pass
+
+
+class Sadad(Source):
+    pass
+
+
+class Payment(Resource, Refund):
 
     @classmethod
     def fetch(cls, id):
         payment = super().fetch(id)
-        return cls.fix_source(payment)
+        return Source.build(payment)
 
     @classmethod
     def list(cls, data=None):
         payments = super().list(data)
         fixed_payments = []
         for p in payments:
-            fixed_payments.append(cls.fix_source(p))
+            fixed_payments.append(Source.build(p))
 
         return fixed_payments
 
     def refund(self, amount=None):
         super().refund(amount)
-        __class__.fix_source(self)
-
-
-class Sadad:
-    def __init__(self, **kwargs):
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
-
-
-class CreditCard:
-    def __init__(self, **kwargs):
-        for key in kwargs:
-            setattr(self, key, kwargs[key])
+        __class__.build(self)
