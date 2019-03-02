b6fe44e6c8d85ee0bd9229c7931156641db416dd
nuha <nuhaaziz75@gmail.com>
2019-03-02 17:59:10 +0300

    invoice test mods

--- tests/server_stubs.py
+++ tests/server_stubs.py
@@ -23,9 +23,9 @@ API_URI = r"api(mig)?\.moyasar\.com\/v1\/.*"
 
 def stub_server_request(method, url, resource, status=200):
     response = ''
-
+    prepare_json = json.dumps(resource)
     if status in range(200, 201):
-        response = json.loads(open(resource).read())
+        response = json.loads(prepare_json)
     elif status in range(400, 429):
         response = {"type": "authentication_error", "message": "Invalid authorization credentials", "errors": None, "http_code": status}
         if status == 401:
@@ -36,3 +36,7 @@ def stub_server_request(method, url, resource, status=200):
     httpretty.enable()
     httpretty.reset()
     httpretty.register_uri(method.upper(), url, body=json.dumps(response), status=status)
+
+
+def remove_stub():
+    httpretty.disable()
