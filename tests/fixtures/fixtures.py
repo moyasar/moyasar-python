11c2b135455b1001c2a83ad470e57b0a38446c07
nuha <nuhaaziz75@gmail.com>
2019-03-03 10:42:12 +0300

    payment tests

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -4,7 +4,8 @@ invoice = {
     "amount": "1000",
     "currency": "SAR",
     "description": "Test",
-    "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original/data?1471819842",
+    "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original/data"
+                "?1471819842",
     "amount_format": "10.00 SAR",
     "url": "https://dashboard.moyasar.com/invoices/1b82356d-b5fd-46f8-bde9-3680d62f289a",
     "created_at": "2016-10-15T14:23:49.587Z",
@@ -20,7 +21,8 @@ invoices = {
       "amount": "1000",
       "currency": "SAR",
       "description": "Test",
-      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original/data?1471819842",
+      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original"
+                  "/data?1471819842",
       "amount_format": "10.00 SAR",
       "url": "https://dashboard.moyasar.com/invoices/1b82356d-b5fd-46f8-bde9-3680d62f289a",
       "created_at": "2016-10-15T14:23:49.587Z",
@@ -33,7 +35,8 @@ invoices = {
       "amount": "1000",
       "currency": "SAR",
       "description": "Test",
-      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original/data?1471819842",
+      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original"
+                  "/data?1471819842",
       "amount_format": "10.00 SAR",
       "url": "https://dashboard.moyasar.com/invoices/1b82356d-b5fd-46f8-bde9-3680d62f289a",
       "created_at": "2016-10-15T14:23:49.587Z",
@@ -46,7 +49,8 @@ invoices = {
       "amount": "1000",
       "currency": "SAR",
       "description": "Test",
-      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original/data?1471819842",
+      "logo_url": "https://s3-eu-west-1.amazonaws.com/moyasar.api.assets.prod/entities/logos/ec8/299/59-/original"
+                  "/data?1471819842",
       "amount_format": "10.00 SAR",
       "url": "https://dashboard.moyasar.com/invoices/1b82356d-b5fd-46f8-bde9-3680d62f289a",
       "created_at": "2016-10-15T14:23:49.587Z",
@@ -55,6 +59,31 @@ invoices = {
     }
   ]
 }
+
+payment = {
+    "id": "328f5dca-91ec-435d-b13f-86052a1d0f7b",
+    "status": "initiated",
+    "amount": 1000,
+    "fee": 0,
+    "currency": "SAR",
+    "refunded": 0,
+    "refunded_at": "null",
+    "description": "Test",
+    "amount_format": "10.00 SAR",
+    "fee_format": "0.00 SAR",
+    "invoice_id": "null",
+    "ip": "78.95.12.65",
+    "created_at": "2016-10-15T14:23:46.214Z",
+    "updated_at": "2016-10-15T14:23:47.426Z",
+    "source": {
+      "type": "sadad",
+      "username": "u3041555Xolp",
+      "error_code": "null",
+      "message": "null",
+      "transaction_id": "null",
+      "transaction_url": "null"
+    }
+  }
 
 payments = {
     "payments": [
--- tests/server_stubs.py
+++ tests/server_stubs.py
@@ -7,21 +7,7 @@ import httpretty
 API_URI = r"api(mig)?\.moyasar\.com\/v1\/.*"
 
 
-# FIXTURE_DIR = os.path.join(
-#     os.path.dirname(os.path.realpath(__file__)),
-#     'fixtures',
-# )
-#
-#
-# @pytest.mark.datafiles(
-#     os.path.join(FIXTURE_DIR, 'invoice.json'),
-#     os.path.join(FIXTURE_DIR, 'invoices.json'),
-#     os.path.join(FIXTURE_DIR, 'payment.json'),
-#     os.path.join(FIXTURE_DIR, 'payments.json'),
-# )
-
-
-def stub_server_request(method, url, resource, status=200):
+def stub_server_request(method, url, resource, status=200, error_message= None, errors= None):
     response = ''
     prepare_json = json.dumps(resource)
     if status in range(200, 201):
@@ -29,9 +15,9 @@ def stub_server_request(method, url, resource, status=200):
     elif status in range(400, 429):
         response = {"type": "authentication_error", "message": "Invalid authorization credentials", "errors": None, "http_code": status}
         if status == 401:
-            response.update({"type": "authentication_error", "http_code": 401})
+            response.update({"type": "authentication_error"})
         if status == 400:
-            response.update({"type": "invalid_request_error", "http_code": 400})
+            response.update({"type": "invalid_request_error", "message": error_message, "errors": errors})
 
     httpretty.enable()
     httpretty.reset()
